3aa371632cb8a00a0ea0bf121fc9fea233981a28
Ivan Milenin <i.milenin@lipt-soft.ru>
2024-04-08 23:35:50 +0300

    [no-task] generate CatalogInteractorTest

--- modules-app/domain/build.gradle.kts
+++ modules-app/domain/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.library")
     kotlin("plugin.serialization")
     id("org.jetbrains.kotlinx.kover")
+    alias(libs.plugins.mockmp)
 }
 
 private val iosBaseName = "base.domain"
@@ -38,6 +39,15 @@ kotlin {
                 implementation(project(":modules-app:core-ui"))
                 // Add here you dependencies
             }
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test-junit"))
+            implementation(kotlin("test-common"))
+            implementation(kotlin("test-annotations-common"))
+            implementation(libs.bundles.commonTest)
+
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0") // Check for the latest version
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:1.8.0") // Check for the latest version
         }
         val androidMain by getting {
             dependencies {
@@ -78,4 +88,8 @@ android {
     kotlin {
         jvmToolchain(17)
     }
+}
+mockmp {
+    usesHelper = true
+    installWorkaround()
 }
